4b4772b1a1dfbf6937f819fb1f600467b8e6215f
famtechdu <dovandu9x03@gmail.com>
2019-06-10 11:31:30 +0700

    create new project

---
+++ android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapOptionsSink.java
@@ -0,0 +1,40 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.googlemaps;
+
+import com.google.android.gms.maps.model.LatLngBounds;
+
+/** Receiver of GoogleMap configuration options. */
+interface GoogleMapOptionsSink {
+  void setCameraTargetBounds(LatLngBounds bounds);
+
+  void setCompassEnabled(boolean compassEnabled);
+
+  void setMapType(int mapType);
+
+  void setMinMaxZoomPreference(Float min, Float max);
+
+  void setRotateGesturesEnabled(boolean rotateGesturesEnabled);
+
+  void setScrollGesturesEnabled(boolean scrollGesturesEnabled);
+
+  void setTiltGesturesEnabled(boolean tiltGesturesEnabled);
+
+  void setTrackCameraPosition(boolean trackCameraPosition);
+
+  void setZoomGesturesEnabled(boolean zoomGesturesEnabled);
+
+  void setMyLocationEnabled(boolean myLocationEnabled);
+
+  void setMyLocationButtonEnabled(boolean myLocationButtonEnabled);
+
+  void setInitialMarkers(Object initialMarkers);
+
+  void setInitialPolygons(Object initialPolygons);
+
+  void setInitialPolylines(Object initialPolylines);
+
+  void setInitialCircles(Object initialCircles);
+}
